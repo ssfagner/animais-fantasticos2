1e85eda995856afa9edce68e1339c13e5659141e
Fagner Santos <fagner.0001@outlook.com>
2021-01-22 21:01:42 -0300

    Fetch animais refatorado

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ import Tooltip from './modules/tooltip.js';
 import initDropdownMenu from './modules/dropdown-menu.js';
 import initMenuMobile from './modules/menu-mobile.js';
 import initFuncionamento from './modules/funcionamento.js';
-import initFetchAnimais from './modules/fetch-animais.js';
+import fetchAnimais from './modules/fetch-animais.js';
 import fetchBitcoin from './modules/fetch-bitcoin.js';
 import initAnimacaoScroll from './modules/scroll-animacao.js';
 
@@ -28,6 +28,7 @@ tooltip.init();
 initDropdownMenu();
 initMenuMobile();
 initFuncionamento();
-initFetchAnimais();
 initAnimacaoScroll();
 fetchBitcoin('https://blockchain.info/ticker', '.btc-preco');
+
+fetchAnimais('../../animaisapi.json', '.numeros-grid');
